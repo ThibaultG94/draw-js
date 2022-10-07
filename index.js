8e7cfa0d450e0c2f4e5b496bcf8556d226d99a3e
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-10-07 19:37:37 +0200

    instructions

---
+++ index.js
@@ -0,0 +1,9 @@
+// On pointe le canvas et on créer ctx
+
+// Fonction getMousePos -> pour connaitre la position de la souris
+
+// Fonction mouseMove -> pour dessiner quand on bouge la souris
+
+// addEventListener -> canvas
+
+// addEventListener -> reset
